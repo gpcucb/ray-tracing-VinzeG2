fb400dbd3b4d1c8a2d4ee5c20ad7437b0d7ec5f2
Vinze Gonzales <vinzegonzales@Vinzes-iMac.local>
2017-02-21 21:19:25 -0400

    class RGB created

---
+++ RGB.cpp
@@ -0,0 +1,14 @@
+//
+//  RGB.cpp
+//  ray-tracing-VinzeG2
+//
+//  Created by Vinze Gonzales on 2/21/17.
+//  Copyright © 2017 VG2. All rights reserved.
+//
+
+#include "RGB.hpp"
+
+RGB::RGB(){}
+
+RGB::~RGB(){}
+
---
+++ RGB.hpp
@@ -0,0 +1,25 @@
+//
+//  RGB.hpp
+//  ray-tracing-VinzeG2
+//
+//  Created by Vinze Gonzales on 2/21/17.
+//  Copyright © 2017 VG2. All rights reserved.
+//
+
+#ifndef RGB_hpp
+#define RGB_hpp
+
+#include <stdio.h>
+
+class RGB {
+private:
+    float red;
+    float green;
+    float blue;
+    
+public:
+    RGB();
+    ~RGB();
+};
+
+#endif /* RGB_hpp */
