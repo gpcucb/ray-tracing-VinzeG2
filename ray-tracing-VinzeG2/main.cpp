0f758b4f600a1eb23ca3f5b0019f17b71aaa2fe3
Vinze Gonzales <vinzegonzales@vinzes-imac.local>
2017-02-21 21:12:01 -0400

    Initial Commit

---
+++ ray-tracing-VinzeG2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ray-tracing-VinzeG2
+//
+//  Created by Vinze Gonzales on 2/21/17.
+//  Copyright © 2017 VG2. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
